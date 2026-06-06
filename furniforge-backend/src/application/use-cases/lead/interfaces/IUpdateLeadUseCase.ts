7ad423d1b1254b073f521501f2a156b3e6afe68d
Aishwarya <aiswaryalakshmikm000@gmail.com>
2026-06-06 10:59:31 +0530

    feat: implement admin delete designer

--- furniforge-backend/src/application/use-cases/lead/interfaces/IUpdateLeadUseCase.ts
+++ furniforge-backend/src/application/use-cases/lead/interfaces/IUpdateLeadUseCase.ts
@@ -1,5 +1,6 @@
-import { UpdateLeadFDTO, UpdateLeadResponseDTO } from "../../../dtos/lead/UpdateLeadDTO";
+import { LeadCommandResponseDTO } from "../../../dtos/lead/LeadCommandResponseDTO";
+import { UpdateLeadFDTO } from "../../../dtos/lead/UpdateLeadDTO";
 
 export interface IUpdateLeadUseCase {
-  execute( leadId: string, dto: UpdateLeadFDTO ): Promise<UpdateLeadResponseDTO>;
+  execute( leadId: string, dto: UpdateLeadFDTO ): Promise<LeadCommandResponseDTO>;
 }
